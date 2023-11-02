f90f30317814b57f83d7e7736b35ecea32582aeb
06bisik03 <06bisik03@gmail.com>
2023-11-02 06:52:27 +0100

    added kinoscmetics css

--- src/intro/Portfolio.js
+++ src/intro/Portfolio.js
@@ -7,7 +7,9 @@ const Portfolio = () => {
     },
     { project_img: "/images/hiPex.png", project_link: "https://astounding-pika-903876.netlify.app" },
     { project_img: "/images/flavor.png", project_link: "https://sensational-pixie-1acf57.netlify.app" },
-    {project_img: "/images/kinosmetics.png", project_link: "https://dapper-tanuki-24b6f6.netlify.app"}
+    {project_img: "/images/kinosmetics.png", project_link: "https://dapper-tanuki-24b6f6.netlify.app"},
+    {project_img: "/images/comingsoon.jpeg",project_link: "/"},
+    {project_img: "/images/comingsoon.jpeg",project_link: "/"},
   ];
 
   return (
